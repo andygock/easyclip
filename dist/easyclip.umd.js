aa998f241a55375b78f3c708b6a084a8d959987a
Andy Gock <andy@gock.net>
2020-07-26 19:23:49 +1000

    Update dist

--- dist/easyclip.umd.js
+++ dist/easyclip.umd.js
@@ -1,23 +1,13 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.easyclip = factory());
-}(this, (function () { 'use strict';
+(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?module.exports=f():typeof define==='function'&&define.amd?define(f):(g=typeof globalThis!=='undefined'?globalThis:g||self,g.easyclip=f());}(this,(function(){'use strict';const copy = (str) => {
+  // create dummy textarea with copy string inside
+  const el = document.createElement("textarea");
+  el.setAttribute("readonly", "");
+  el.style.display = "hidden";
+  el.value = str;
 
-  const copy = (str) => {
-    // create dummy textarea with copy string inside
-    const el = document.createElement("textarea");
-    el.setAttribute("readonly", "");
-    el.style.display = "hidden";
-    el.value = str;
-
-    // add dummy element to DOM, select and copy to clipboard
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
-  };
-
-  return copy;
-
-})));
+  // add dummy element to DOM, select and copy to clipboard
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};return copy;})));
