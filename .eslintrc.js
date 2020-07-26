e18bfa87186fc46ca38af620adceac05c8dcb53c
Andy Gock <andy@gock.net>
2020-07-26 16:35:00 +1000

    Some re-organisation, use rollup.config.js

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/easyclip.umd.js',
+    format: 'umd',
+    name: 'easyclip',
+    compact: true,
+  },
+};
